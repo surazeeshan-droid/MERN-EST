d3928ad87f2e296b6bc789e52efcc42773a2ced5
Umar <surazeeshan.droid@gmail.com>
2025-11-12 14:53:32 -0600

    install react js and tailwindcss and  create the first template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <h1 className='text-red-400'>App</h1>
+    )
+  }
+}
+
+export default App
