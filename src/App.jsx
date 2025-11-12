0e9d3adb795ba8eb88c2fa1136baa9ebb793120b
Umar <surazeeshan.droid@gmail.com>
2025-11-12 17:57:13 -0600

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import { BrowserRouter,Routes,Route  } from 'react-router-dom';
+import Home from'./pages/Home';
+import SignIn from'./pages/SignIn';
+import SignUp from'./pages/SignUp';
+import About from'./pages/About';
+import Profile from'./pages/Profile';
 
-export class App extends Component {
-  render() {
+export default function App () {
     return (
-      <h1 className='text-red-400'>App</h1>
-    )
+    < BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+       <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />
+         <Route path="/about" element={<About/>} />
+          <Route path="/profile" element={<Profile/>} />
+    </Routes>
+    </BrowserRouter>
+     
+    );
   }
-}
 
-export default App
