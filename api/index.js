63322b247b224b8ab51d88304d05000e20732c7c
Umar <surazeeshan.droid@gmail.com>
2025-11-19 13:22:37 -0600

    complete sign up page ui

--- api/index.js
+++ api/index.js
@@ -1,32 +1,31 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import  userRouter  from './routes/user.route.js';
-import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=> {
-    console.log('Connected to MongoDB!');
-}).catch((err)=>{
-    console.log(err);
+mongoose.connect(process.env.MONGO)
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch(err => console.log(err));
 
-});
-
-const app= express();
+const app = express();
 app.use(express.json());
 
-app.listen(3000,()=>{
-    console.log('Sever is running on port 3000!');
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-app.use((err,req,res,next)=>{
-    const statusCode = err.statusCode ||500;
-    const message = err.message||'internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
